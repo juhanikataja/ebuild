71359e126d05ff9dc5a9ef03e692c0e8c208eb00
Juhani Kataja <juhani.kataja@csc.fi>
2025-04-10 14:25:14 +0300

    multi cmake extra options and fix query_remove

--- build.sh
+++ build.sh
@@ -106,7 +106,7 @@ while getopts "s:C:chirbB:mn:O:M:t:p:" opt; do
       source ${OPTARG}
       ;;
     O)
-      CMAKE_OPTIONS=${OPTARG}
+      CMAKE_OPTIONS="${CMAKE_OPTIONS} ${OPTARG}"
       >&2 echo "* Extra cmake options: \"${CMAKE_OPTIONS}\""
       ;;
     C)
@@ -194,7 +194,7 @@ done
 if [ ${DO_CLEAN} -eq 1 ]; then
   find ${ELMER_BUILD_DIR} -maxdepth 1 -print0 | xargs -0 -n 1 ls -l -d
   read -p "Remove these recursively? (y/n)" query_remove
-  if [ $query_remove="y" ]; then
+  if [ "$query_remove" = "y" ]; then
     find ${ELMER_BUILD_DIR} -maxdepth 1 -print0 | xargs -0 -n 1 -I % sh -c 'echo Removing %; rm -r %'
   fi
   mkdir -p ${ELMER_BUILD_DIR} && cd ${ELMER_BUILD_DIR}
